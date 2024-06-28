bf1ba2bed804175bb2826720cb9012218684d0de
ninnitron <giuseppe.salamone.dev@gmail.com>
2024-06-28 18:16:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <!-- jQuery -->
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <title>Test PHP</title>
+        <title>PHP Test</title>
     </head>
     <body>
         <div class="container-fluid p-3" id="content"></div>
